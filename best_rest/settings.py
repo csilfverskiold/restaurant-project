843a47367ca2d48ba91e5479f7057f44224b6c63
csilfverskiold <christian.silfverskiold@gmail.com>
2023-01-26 11:21:12 +0000

    Create superuser

--- best_rest/settings.py
+++ best_rest/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'booking',
+    'booking',  # Added Django app name
 ]
 
 MIDDLEWARE = [
@@ -133,7 +133,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
