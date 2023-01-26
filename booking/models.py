d2bb34e7e5408b54220dbda3ea1ddab107ce188a
csilfverskiold <christian.silfverskiold@gmail.com>
2023-01-26 21:35:58 +0000

    Add basic structure for model class Reservation

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Import this later when needed- from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Reservation (models.Model):
+    name = models.CharField(max_length=200)
