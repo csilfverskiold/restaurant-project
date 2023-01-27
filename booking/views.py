9e1047ea58b24cbc6e76bb5be3369a9285adbcf6
csilfverskiold <christian.silfverskiold@gmail.com>
2023-01-27 09:30:58 +0000

    Add routes for home and booking

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# This function handles the traffic from the restaurant home page
+def home(request):
+    return HttpResponse('<h1>Restaurant Home</h1>')
+
+
+# This function handles the traffic from the restaurant booking page
+def booking(request):
+    return HttpResponse('<h1>Restaurant Booking</h1>')
