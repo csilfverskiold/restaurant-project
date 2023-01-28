acd15d9f037902d416aef52d3e191383420486e4
csilfverskiold <christian.silfverskiold@gmail.com>
2023-01-28 13:31:54 +0000

    Add view, urls, basic template for Reservation

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='restaurant-home'),  # Home url
-    path('booking/', views.booking, name='restaurant-booking'),  # Booking url
+    path('booking/', views.ReservationCreate.as_view(),
+         name='restaurant-booking'),  # Booking url
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from .models import Reservation
 
 
 # This function handles the traffic from the restaurant home page
@@ -7,6 +9,7 @@ def home(request):
     return HttpResponse('<h1>Restaurant Home</h1>')
 
 
-# This function handles the traffic from the restaurant booking page
-def booking(request):
-    return HttpResponse('<h1>Restaurant Booking</h1>')
+class ReservationCreate(CreateView):  # Renders Reservation to create view
+    model = Reservation
+    fields = (['service', 'guest', 'day', 'time',
+               'content', 'first_name', 'last_name', 'email'])
