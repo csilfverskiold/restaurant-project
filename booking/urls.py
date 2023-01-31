7dc81b15fd1c079b1c14e7d9beb6c9c0e9586067
csilfverskiold <christian.silfverskiold@gmail.com>
2023-01-31 07:56:30 +0000

    Remove ListView, add DetailView

--- booking/urls.py
+++ booking/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='restaurant-home'),  # Home url
-    path('booking/', views.ReservationCreate.as_view(),
-         name='restaurant-booking'),  # Booking url
-    path('list/', views.ReservationList.as_view(),
-         name='restaurant-list'),  # List url
-    path('update/', views.ReservationUpdate.as_view(),
-         name='restaurant-update'),  # Update url
+    path('booking/', views.ReservationCreate.as_view(),  # Booking url
+         name='restaurant-booking'),
+    path('<pk>/', views.ReservationDetailView.as_view(),  # Detail url
+         name='restaurant-detail'),
+    path('update/<int:pk>/', views.ReservationUpdate.as_view(),  # Update url
+         name='restaurant-update'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView
-from django.views import generic
+from django.views.generic.detail import DetailView
 from .models import Reservation
 
 
@@ -10,17 +10,26 @@ def home(request):
     return HttpResponse('<h1>Restaurant Home</h1> <a href="https://8000-csilfverski-restaurantp-dv4iba0kkur.ws-eu84.gitpod.io/booking/">Reservation</a>')
 
 
-class ReservationCreate(CreateView):  # Renders Reservation to create view
+class ReservationCreate(CreateView):  # Renders view to create a reservation
     model = Reservation
     fields = (['guest', 'day', 'time',
                'content', 'first_name', 'last_name', 'email'])
 
 
-class ReservationList(generic.ListView):
+class ReservationDetailView(DetailView):  # Renders view details of a reservation
     model = Reservation
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ReservationDetailView,
+                        self).get_context_data(*args, **kwargs)
+        context["category"] = "MISC"
+        return context
+    # "By default DetailView will only display fields of a table.
+    # If one wants to modify this context data before sending it
+    # to template or add some extra field, context data can be overridden."
 
-class ReservationUpdate(UpdateView):
+
+class ReservationUpdate(UpdateView):  # Renders view to update a reservation
     model = Reservation
     fields = (['guest', 'day', 'time',
                'content', 'first_name', 'last_name'])
