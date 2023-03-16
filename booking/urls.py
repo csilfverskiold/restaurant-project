eb54a7e0f42a1fd22b7902754bb92abbcc601fc7
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-16 10:52:56 +0000

    Edit urls, edit basic template content in base.html and index.html

--- booking/urls.py
+++ booking/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),  # Home url
     path('booking/', views.ReservationCreate.as_view(),  # Booking url
          name='restaurant-booking'),
-    path('<pk>/', views.ReservationDetailView.as_view(),  # Detail url
+    path('booking/<pk>/', views.ReservationDetailView.as_view(),  # Detail url
          name='restaurant-detail'),
-    path('<pk>/update', views.ReservationUpdateView.as_view(),  # Update url
+    path('booking/<pk>/update', views.ReservationUpdateView.as_view(),  # Update url
          name='restaurant-update'),
 ]
