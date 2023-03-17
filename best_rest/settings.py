4441bd3370eb9a8fca935e2256fb36914105e116
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-17 09:34:14 +0000

    Add basic templates and functionality for user log in/out

--- best_rest/settings.py
+++ best_rest/settings.py
@@ -140,3 +140,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
